191796a94ec81b851719fe7a3f7d09e97103cba2
HYUNIL-DESKTOP <kimhyunil20@naver.com>
2018-05-14 23:09:39 +0900

    itemlistActivity add

---
+++ Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/ItemlistActivity.java
@@ -0,0 +1,14 @@
+package com.example.kimhy.open_source_project_naver_price_compare;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ItemlistActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.itemlist);
+    }
+}

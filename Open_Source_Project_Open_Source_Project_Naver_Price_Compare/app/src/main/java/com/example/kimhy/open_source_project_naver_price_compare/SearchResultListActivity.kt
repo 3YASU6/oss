3498f73c35b296bdcd754ce35ad22707f0d86846
YANOSUHI <3suhi6@gmail.com>
2018-05-23 19:44:53 +0900

    화면 재생성 및 설정

---
+++ Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/SearchResultListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kimhy.open_source_project_naver_price_compare
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchResultListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_result_list)
+    }
+}
---
+++ Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/ShowMoreItemInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kimhy.open_source_project_naver_price_compare
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowMoreItemInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_more_item_info)
+    }
+}

83c3030a19e9993c3c970cac963bc360c9c0d96e
YANOSUHI <3suhi6@gmail.com>
2018-05-24 19:35:34 +0900

    화면 및 전이 기능 추가

--- Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/ItemSearchActivity.kt
+++ Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/ItemSearchActivity.kt
@@ -1,12 +1,28 @@
 package com.example.kimhy.open_source_project_naver_price_compare
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_item_search.*
 
 class ItemSearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_search)
+
+        val listView = findViewById(R.id.searchListView) as ListView
+        val data_array_items = Array(20, { i -> "Title-$i" })
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data_array_items)
+        listView.adapter = adapter
+
+        searchListView.setOnItemClickListener { adapterView, view, position, id ->
+
+            val intent = Intent(this, ShowMoreItemInfoActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
--- Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/itemlist.kt
+++ Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/itemlist.kt
@@ -1,9 +1,13 @@
 package com.example.kimhy.open_source_project_naver_price_compare
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.util.Log
+import android.view.View
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_itemlist.*
+
 
 class itemlist : AppCompatActivity() {
 
@@ -14,9 +18,33 @@ class itemlist : AppCompatActivity() {
         setContentView(R.layout.activity_itemlist)
 
         val listView = findViewById(R.id.listView) as ListView
-        val dataArray = arrayOf("yano","suhi","sai","kou","yana")
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataArray)
+        //val dataArray = arrayOf("yano","suhi","sai","kou","yana")
+        val data_array_items = Array(20, { i -> "Title-$i" })
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data_array_items)
         listView.adapter = adapter
+
+        // item click시 발생하는 event를 추가
+        listView.setOnItemClickListener { adapterView, view, position, id ->
+            // Toast 표시
+            val textView = view.findViewById<TextView>(android.R.id.text1)
+            Toast.makeText(this, "Clicked: ${textView.text}", Toast.LENGTH_SHORT).show()
+
+            // activity_graph 화면에 이동
+            val intent = Intent(this, GraphActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        val hoge = "yanosuhi"
+        // add button click시 발생하는 event를 추가
+        addButton.setOnClickListener {
+            //Log.v(hoge, "clicked")
+
+            // activity_graph 화면에 이동
+            val intent = Intent(this, ItemSearchActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
 

44dd859c2c5433ef69f17c7fb32cae3d803fa287
HYUNIL <kimhyunil2020@gmail.com>
2018-05-24 20:39:02 +0900

    메니페스트 오류로 추정됨 android.content.ActivityNotFoundException: Unable to find explicit activity class {com.example.kimhy.open_source_project_naver_price_compare/int}; have you declared this activity in your AndroidManifest.xml?

--- Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/itemlist.kt
+++ Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/itemlist.kt
@@ -1,29 +1,23 @@
 package com.example.kimhy.open_source_project_naver_price_compare
 
-/*
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
-import android.util.Log
-import android.view.View
-import android.widget.*
-import kotlinx.android.synthetic.main.activity_itemlist.*
-
-=======
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import android.widget.ListView
->>>>>>> 72594f7f731029be579f33a5ec72f57cdac745df
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_itemlist.*
+
 
 class ItemList : AppCompatActivity() {
 
 
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_itemlist)
+
 
         val listView = findViewById(R.id.listView) as ListView
         //val dataArray = arrayOf("yano","suhi","sai","kou","yana")
@@ -51,15 +45,11 @@ class ItemList : AppCompatActivity() {
             // activity_graph 화면에 이동
             val intent = Intent(this, ItemSearchActivity::class.java)
             startActivity(intent)
+
         }
 
-    }
-    fun onClick(view:View?) {
-        val intent = Intent(this, ItemSearchActivity::class.java)
-        startActivity(intent)
     }
 
 
 }
 
-        */

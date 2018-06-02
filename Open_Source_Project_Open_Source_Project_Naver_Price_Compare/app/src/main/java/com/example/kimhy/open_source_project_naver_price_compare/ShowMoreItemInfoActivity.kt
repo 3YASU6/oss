2a44f704977dda2c8e26662d8c8104b477bfb924
YANOSUHI <3suhi6@gmail.com>
2018-06-02 13:54:54 +0900

    검색결과 상품 상세정보 표시 화면 구현

--- Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/ShowMoreItemInfoActivity.kt
+++ Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/ShowMoreItemInfoActivity.kt
@@ -1,12 +1,20 @@
 package com.example.kimhy.open_source_project_naver_price_compare
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_show_more_item_info.*
 
 class ShowMoreItemInfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_more_item_info)
+
+        // addToWishListButton click시 발생하는 event를 추가
+        addToWishListButton.setOnClickListener {
+            val intent = Intent(this, ItemListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

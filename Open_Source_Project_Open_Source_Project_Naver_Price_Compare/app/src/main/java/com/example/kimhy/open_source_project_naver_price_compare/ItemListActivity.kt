fae203085945659e9735b3e9451cc709617d75c9
Ella <ella.ayuningtyas.eru@gmail.com>
2018-06-10 17:57:30 +0900

    get data (아직 성공하지 않아요)

--- Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/ItemListActivity.kt
+++ Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/ItemListActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kimhy.open_source_project_naver_price_compare
 
 import android.content.Intent
+import android.nfc.Tag
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,9 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import kotlinx.android.synthetic.main.activity_item_list.*
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -16,6 +20,13 @@ import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_show_more_item_info.*
 import org.json.JSONException
 import kotlin.coroutines.experimental.EmptyCoroutineContext
+import com.google.firebase.firestore.QueryDocumentSnapshot
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.DocumentSnapshot
+
+
+
+
 
 
 class ItemListActivity : AppCompatActivity() {
@@ -28,6 +39,8 @@ class ItemListActivity : AppCompatActivity() {
         db = FirebaseFirestore.getInstance().document("items/detail")
         val thisView = findViewById(R.id.listView) as ListView
         val data_array_items = Array(20, { i -> "Title-$i" })
+        val listvieww = Array(20, {  })
+
 
         val aa:String
         val i_price:String
@@ -35,30 +48,32 @@ class ItemListActivity : AppCompatActivity() {
         val intent = intent
         val bd = intent.extras
         val ti = intent.getStringExtra("title")
-        val listview = ArrayList<String>()
+
 
         //===============================================
 //set the text in the textview
         if (bd != null) {
             val getName = bd.get("title") as String?
             trytext.setText(getName)
-//            val getiprice = bd.get("iprice") as String
-//            lpriceText.setText(getiprice)
-//            val getmallname = bd.get("mallname") as String
-//            mallNameText.setText(getmallname)
+            val get_iprice = bd.get("iprice") as String?
+            trytextprice.setText(get_iprice)
+            val get_imall = bd.get("mallname") as String?
+            trytextmall.setText(get_imall)
+
 
             var flavour = trytext.text.toString().trim()
             aa = trytext.getText().toString()
-            i_price = trytext.getText().toString()
-            mall_name = trytext.getText().toString()
+            i_price = trytextprice.getText().toString()
+            mall_name = trytextmall.getText().toString()
+
 
            //========================================
- //add to database
+ //==============add and insert to database
             if (flavour!=null) {
                 try {
                     val items = HashMap<String, Any>()
            //         items.put("detail", trytext)
-                    db.collection(aa).document("detail").set(items).addOnSuccessListener {
+                    db.collection(aa).document("detail").collection(i_price).document("detailprice").set(items).addOnSuccessListener {
                         void: Void? -> Toast.makeText(this, "Successfully uploaded to the database :)", Toast.LENGTH_LONG).show()
                     }.addOnFailureListener {
                         exception: java.lang.Exception -> Toast.makeText(this, exception.toString(), Toast.LENGTH_LONG).show()
@@ -72,9 +87,40 @@ class ItemListActivity : AppCompatActivity() {
 
 
 
+//=======================================================================================================================================
+            //==================get data from database (ERROR)
+            val docReff = db.collection("items").document("detail")
+            docReff.get().addOnCompleteListener(OnCompleteListener<DocumentSnapshot> { task ->
+                if (task.isSuccessful) {
+                    val document = task.result
+                    if (document != null) {
 
+                        Log.d("TAG", "before")
+                        //try to print data
+                        for ( i in listvieww.indices) {
+                            println(task.result)
+                            Toast.makeText(this, "DocumentSnapshot data: " + listvieww[i]
+                                    , Toast.LENGTH_LONG).show()
+                        }
 
+                        Log.d("TAG", "message")
 
+                    } else {
+                        Toast.makeText(this, "no such a data", Toast.LENGTH_LONG).show()
+
+                    }
+                } else {
+                    Toast.makeText(this, "get failed with" + task.exception, Toast.LENGTH_LONG).show()
+                }
+            })
+
+        //=================== custom object
+            val docRef = db.collection("items").document("detail")
+            docRef.get().addOnSuccessListener(OnSuccessListener<DocumentSnapshot> { documentSnapshot ->
+                val note = documentSnapshot.toObject(ItemListActivity::class.java)
+            })
+
+//================================================================================================================
 
 
 

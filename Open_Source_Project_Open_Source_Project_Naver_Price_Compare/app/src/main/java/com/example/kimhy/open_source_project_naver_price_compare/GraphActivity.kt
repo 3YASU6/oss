3ec8cc9b610cb0d3a8aaf1caa80bb860e26efce3
DESKTOP-969OV6C\3suhi <3suhi6@gmail.com>
2018-06-10 19:49:55 +0900

    그래프 종류: BarChart → LineChart 변경

--- Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/GraphActivity.kt
+++ Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/GraphActivity.kt
@@ -5,18 +5,24 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.widget.ListView
 import android.widget.Toast
 import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.Chart
+import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
+
 import com.github.mikephil.charting.formatter.PercentFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
 
 import kotlinx.android.synthetic.main.activity_graph.*
 import retrofit2.http.Url
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineRadarDataSet
+
 
 class GraphActivity : AppCompatActivity() {
 
@@ -24,47 +30,46 @@ class GraphActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_graph)
 
-        //그래프작성
-        // 입력 데이터
-        val input = listOf(BarEntry(1f, 50f, "Green"), BarEntry(2f, 20f, "Yellow"), BarEntry(3f, 90f, "Red"), BarEntry(4f, 40f, "Blue"))
-        val set = BarDataSet(input, "예의 설명문")
-        set.colors = ColorTemplate.MATERIAL_COLORS.toList()
-        set.isHighlightEnabled = true //タップで拡大
-        val data = BarData(set)
+        val mChart = findViewById(R.id.chart) as LineChart
 
-        //데이터 서식
-        data.setValueFormatter(PercentFormatter()) //フォーマット
-        data.setValueTextSize(12f) //テキストサイズ
-        data.setValueTextColor(Color.WHITE) //色
-        data.isHighlightEnabled = true
-        bar_chart.data = data
+        // 터치 제스처 사용
+        mChart.setTouchEnabled(true)
 
-        //그래프의 UI설정
-        bar_chart.legend.isEnabled = true //凡例の表示
+        // 크기 조정 및 드래그 사용
+        mChart.setDragEnabled(true)
+        mChart.setScaleEnabled(true)
+        mChart.setDrawGridBackground(false)
 
-        // X측
-        val xAxis = bar_chart.xAxis
-        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        // 비활성화 된 경우 배율을 x 축과 y 축에서 개별적으로 수행 할 수 있습니다.
+        mChart.setPinchZoom(true)
 
-        // Y측(우)
-        val rAxis = bar_chart.axisRight
-        rAxis.isEnabled = false
-        rAxis.mAxisMaximum = 80f
-        rAxis.mAxisMinimum = 10f
+        // 대체 배경색을 설정
+        mChart.setBackgroundColor(Color.LTGRAY)
 
-        // Y측(좌)
-        val lAxis = bar_chart.axisLeft
-        lAxis.textColor = Color.MAGENTA
+        val data = LineData()
+        data.setValueTextColor(Color.BLACK)
 
-        //설명문
-        val description = Description()
-        description.text = "그래프의 설명문"
-        bar_chart.description = description
-        bar_chart.invalidate()
+        // 데이터 추가
+        mChart.setData(data)
 
-        // 애니메이션
-        //bar_chart.animateY(2000, Easing.EasingOption.EaseInBounce)
+        //  라인의 rabel를 설정
+        val l = mChart.getLegend()
+        l.setForm(Legend.LegendForm.LINE)
+        l.setTextColor(Color.BLACK)
 
+        val xl = mChart.getXAxis()
+        xl.setTextColor(Color.BLACK)
+        //xl.setLabelsToSkip(9)
+
+        val leftAxis = mChart.getAxisLeft()
+        leftAxis.setTextColor(Color.BLACK)
+        leftAxis.setAxisMaxValue(3.0f)
+        leftAxis.setAxisMinValue(-3.0f)
+        leftAxis.setStartAtZero(false)
+        leftAxis.setDrawGridLines(true)
+
+        val rightAxis = mChart.getAxisRight()
+        rightAxis.setEnabled(false)
 
 
         //buyButton click시 발생하는 event

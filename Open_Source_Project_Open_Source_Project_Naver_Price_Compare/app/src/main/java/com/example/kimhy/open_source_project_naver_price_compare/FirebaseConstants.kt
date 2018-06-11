d6354862ab7d408561c17722f22ae1588636b28a
HYUNIL-DESKTOP <kimhyunil20@naver.com>
2018-06-11 21:50:02 +0900

    firebase 변수용 파일 추가

---
+++ Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/FirebaseConstants.kt
@@ -0,0 +1,12 @@
+package com.example.kimhy.open_source_project_naver_price_compare
+
+object FirebaseConstants
+{
+    val COLLECTION_PARENT = "items"
+    val DOCUMENT_PARENT = "detail"
+
+    val FIELD_TITLE = "title"
+    val FIELD_IPRICE= "iprice"
+    val FIELD_LINK="link"
+
+}

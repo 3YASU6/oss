d47d2c2748617a2637aa9a69b63ed053f1b8fdf5
YANO_SUHI <3suhi6@gmail.com>
2018-06-13 17:59:29 +0900

    GraphActivity에서 DB관련 코드 comment out

--- Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/GraphActivity.kt
+++ Open_Source_Project_Open_Source_Project_Naver_Price_Compare/app/src/main/java/com/example/kimhy/open_source_project_naver_price_compare/GraphActivity.kt
@@ -5,7 +5,8 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
-import android.widget.ListView
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.Chart
@@ -23,7 +24,6 @@ import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.data.LineRadarDataSet
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import kotlinx.android.synthetic.main.activity_show_more_item_info.*
 
@@ -34,7 +34,7 @@ class GraphActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_graph)
 
-        val l_price:String
+ //       val l_price:String
         val mChart = findViewById(R.id.chart) as LineChart
 
         // 터치 제스처 사용
@@ -53,28 +53,28 @@ class GraphActivity : AppCompatActivity() {
 
         val swapVariable = intent.extras
         if (swapVariable != null) {
-            val getName = swapVariable.get("title") as String
-            titleText.setText(getName)
-            val getiprice = swapVariable.get("iprice") as String
-            priceText.setText(getiprice)
-            val getmallname = swapVariable.get("mallname") as String
-            mallNameText.setText(getmallname)
-            val getimage = swapVariable.get("image") as String
-            println("====================" + getimage)
-            imageurl.loadUrl(getimage)
+//            val getName = swapVariable.get("title") as String
+//            titleText.setText(getName)
+//            val getiprice = swapVariable.get("iprice") as String
+//            priceText.setText(getiprice)
+//            val getmallname = swapVariable.get("mallname") as String
+//            mallNameText.setText(getmallname)
+//            val getimage = swapVariable.get("image") as String
+//            println("====================" + getimage)
+//            imageurl.loadUrl(getimage)
 
-            l_price = lpriceText.getText().toString()
+            // l_price = lpriceText.getText().toString()
             // TODO: FireBase에서 끌고 온 상품 가격변동 정보들을 저장 (가격 = price)  ※지금은 더미
-            val price_from_fireBase = arrayOf(l_price, 900f, 1000f, 1200f, 1200f, 800f, 750f)
+            val price_from_fireBase = arrayOf(900f, 1000f, 1200f, 1200f, 800f, 750f)
             // TODO: FireBase에서 끌고 온 상품 가격변동 정보들을 저장 (날짜 = date)  ※지금은 더미
-            val date_from_fireBase = arrayOf(dateText, 2f, 3f, 4f, 5f, 6f, 7f)
+            val date_from_fireBase = arrayOf(2f, 3f, 4f, 5f, 6f, 7f)
 
             // 그래프의 Point가 되는 데이터 배열
             val xy_data = ArrayList<Entry>()
 
             // FireBase에서 끌고 온 Date, Price를 그래프 Point(x값, y값)로 설정
             for (i in date_from_fireBase.indices) {
-      //          xy_data.add(Entry(date_from_fireBase[i], price_from_fireBase[i])) // xy값(=Entry)를 xy_data에 설정 (X = date_data, Y = price_data)
+                xy_data.add(Entry(date_from_fireBase[i], price_from_fireBase[i])) // xy값(=Entry)를 xy_data에 설정 (X = date_data, Y = price_data)
             }
 
             // xy_data를 LineDataSet인 set에 저장
@@ -109,16 +109,14 @@ class GraphActivity : AppCompatActivity() {
             rightAxis.setEnabled(false)
 
 
+            val buyButton = findViewById(R.id.buyButton) as Button
             //buyButton click시 발생하는 event
             buyButton.setOnClickListener {
-                val webview: String
-                webview = "http://search.shopping.naver.com/gate.nhn?id=12830391037"
-                // 해당 상품에 이동
-                //Naver_API.link
-                //  Uri.parse("https://developer.android.com/reference/android/net/Uri")
-
+                // 해당 상품 구매 사이트에 이동
+                Toast.makeText(this, "해당 상품 구매 사이트에 이동", Toast.LENGTH_SHORT).show()
             }
 
+            val deleteButton = findViewById(R.id.deleteButton) as ImageButton
             //delete button click시 발생하는 event
             deleteButton.setOnClickListener {
                 val builder = AlertDialog.Builder(this)
@@ -130,10 +128,11 @@ class GraphActivity : AppCompatActivity() {
 
 
                     // Toast로 삭제 완료됐음을 표시
-                    Toast.makeText(applicationContext, getString(R.string.deleteFinishedMessage), Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, getString(R.string.deleteFinishedMessage), Toast.LENGTH_SHORT).show()
                 }
                 builder.setNeutralButton("Cancel") { _, _ ->
                 }
+
                 val dialog: AlertDialog = builder.create()
                 dialog.show()
 
